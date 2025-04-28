84c8b0de7e894502e2e9bcce2a4b3426c38713fc
leticiafdepaula <leticiaferreira150998@gmail.com>
2025-04-27 21:54:38 -0300

    feat: pasta module

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+],
+providers: [],
+bootstrap:[AppComponent]
+})
+export class AppModule { }
