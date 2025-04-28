2c088763bb71ae5222413f9965c9f6aa538ceead
leticiafdepaula <leticiaferreira150998@gmail.com>
2025-04-27 21:23:05 -0300

    inlineTemplete desabilitado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: 'app.component.html'
+   
 })
 export class AppComponent {
   title = 'helpDesk';
